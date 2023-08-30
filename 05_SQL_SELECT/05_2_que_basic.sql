fca7736e3e07e20eff8938435a32120ae5f69e68
przygoda <schmidt.multimedia@googlemail.com>
2023-08-30 08:24:57 +0000

    05/2  Basic Queries

---
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -0,0 +1,15 @@
+/* QUERIES Basic */
+
+/*
+Felder abfragen
+Felder kombinieren
+Ausgabe beschränken
+*/
+
+-- Abfrage aller Spalten der Tab
+#SELECT * FROM stocks.ccc;
+
+-- Begrenzung: LIMIT
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombinationen aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
