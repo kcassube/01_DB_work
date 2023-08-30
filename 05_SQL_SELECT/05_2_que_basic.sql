10c588c45ef4bfe3f0a490ad57977c170b42b0b0
przygoda <schmidt.multimedia@googlemail.com>
2023-08-30 08:56:51 +0000

    05/2 CONCAT/LIMIT

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -13,3 +13,14 @@ Ausgabe beschränken
 -- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
 -- Kombinationen aus mehreren Feldern in neues Feld 
 -- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT
+    ticker AS "SYM",
+    c_name AS Unternehmen,
+    #industry Branche
+	#sector Industriesektor
+	CONCAT(sector,"|",industry) AS "Operations"
+FROM stocks.ccc
+LIMIT 10
+#LIMIT 1
+#LIMIT 200,10 -- offset
+;
